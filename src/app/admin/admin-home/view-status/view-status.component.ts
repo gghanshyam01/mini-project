1027328f8191ef5d0bee9bb062ac2870e322bafe
Ghanshyam Gupta <gghanshyam01@gmail.com>
2018-10-29 21:58:25 +0530

    Added Charts

--- src/app/admin/admin-home/view-status/view-status.component.ts
+++ src/app/admin/admin-home/view-status/view-status.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { CustomerDataService } from '../../shared/customer-data.service';
 import { Subscription } from 'rxjs';
+
+import { ChartReadyEvent, ChartErrorEvent } from 'ng2-google-charts';
+
+import { CustomerDataService } from '../../shared/customer-data.service';
 
 // import * as _ from '
 
@@ -10,21 +13,47 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./view-status.component.css']
 })
 export class ViewStatusComponent implements OnInit, OnDestroy {
-  payload: {
-    totalCount: number;
-    assignedCount: number;
-    finishedCount: number;
-    remainingCount: number;
-  }[] = [];
+  isReady = false;
+  msg = 'Loading status chart';
+  pieData = {
+    chartType: 'ColumnChart',
+    dataTable: [['Task', 'Status']],
+    options: {
+      title: 'Customer Assignment Status'
+    }
+  };
 
   statusSub: Subscription;
-  displayedColumns = ['total', 'rCount', 'assCount', 'fCount'];
+  // displayedColumns = ['total', 'rCount', 'assCount', 'fCount'];
   constructor(private custSvc: CustomerDataService) {}
 
   ngOnInit() {
     this.statusSub = this.custSvc.getData().subscribe((data: any) => {
-      this.payload.push(data);
+      // this.payload.push(data);
+      let arr = ['Finished Count', data.finishedCount];
+      this.pieData.dataTable.push(arr);
+
+      arr = ['Assigned Count', data.assignedCount];
+      this.pieData.dataTable.push(arr);
+
+      arr = ['Remaining Count', data.remainingCount];
+      this.pieData.dataTable.push(arr);
+
+      arr = ['Total Count', data.totalCount];
+      this.pieData.dataTable.push(arr);
+
+      this.isReady = true;
     });
+  }
+
+  onReady(res: ChartReadyEvent) {
+    console.log('Ready');
+    this.msg = 'Loading status chart';
+    this.isReady = true;
+  }
+
+  onError(err: ChartErrorEvent) {
+    this.msg = err.message;
   }
 
   ngOnDestroy() {
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminMaterialModule } from './admin-material.module';
+import { Ng2GoogleChartsModule } from 'ng2-google-charts';
 
 import { AdminHomeComponent } from './admin-home/admin-home.component';
 import { ViewStatusComponent } from './admin-home/view-status/view-status.component';
@@ -11,8 +13,6 @@ import { UploadDataComponent } from './admin-home/upload-data/upload-data.compon
 import { AllocateCustomerComponent } from './admin-home/allocate-customer/allocate-customer.component';
 import { DataTableComponent } from './admin-home/allocate-customer/data-table/data-table.component';
 import { CustomerDataService } from './shared/customer-data.service';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-
 
 @NgModule({
   imports: [
@@ -21,7 +21,8 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
     AdminMaterialModule,
     HttpClientModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    Ng2GoogleChartsModule
   ],
   declarations: [
     AdminHomeComponent,
